2b3e68b69035b6dc6a18d9a6aae027f479fb8e5d
dan-schel <me@danschellekens.com>
2024-12-21 16:59:24 +1100

    reorganize eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,57 +4,49 @@ import react from "eslint-plugin-react/configs/recommended.js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
+const customRules = {
+  rules: {
+    // Ignore unused variables if they start with underscores.
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+    // Require === and !==, except when comparing to null.
+    eqeqeq: ["error", "always", { null: "ignore" }],
+
+    // Warn on console.log uses, but allow console.warn.
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+
+    // TODO: Remove this? What does it do?
+    "@typescript-eslint/no-namespace": 0,
+
+    // Warn about prettier violations.
+    "prettier/prettier": "warn",
+  },
+};
+
+const reactConfig = {
+  files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+  ...react,
+  languageOptions: {
+    ...react.languageOptions,
+    globals: {
+      ...globals.serviceworker,
+      ...globals.browser,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
+
 export default tseslint.config(
   {
     ignores: ["node_modules/*", "dist/*", "coverage/*"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    languageOptions: {
-      parserOptions: {
-        warnOnUnsupportedTypeScriptVersion: false,
-        sourceType: "module",
-        ecmaVersion: "latest",
-      },
-    },
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
-    ...react,
-    languageOptions: {
-      ...react.languageOptions,
-      globals: {
-        ...globals.serviceworker,
-        ...globals.browser,
-      },
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
+  reactConfig,
   prettier,
-  {
-    rules: {
-      // Ignore unused variables if they start with underscores.
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
-
-      // Require === and !==, except when comparing to null.
-      eqeqeq: ["error", "always", { null: "ignore" }],
-
-      // Warn on console.log uses, but allow console.warn.
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-
-      // TODO: Remove this? What does it do?
-      "@typescript-eslint/no-namespace": 0,
-
-      // Warn about prettier violations.
-      "prettier/prettier": "warn",
-    },
-  },
+  customRules,
 );
