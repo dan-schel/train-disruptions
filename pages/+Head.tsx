64460941b080b62668d415ed00108cb98b4e1a13
dan-schel <me@danschellekens.com>
2024-12-22 15:59:33 +1100

    generate icons and setup manifest

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -5,8 +5,11 @@ import React from "react";
 export default function HeadDefault() {
   return (
     <>
-      <link rel="icon" href="/favicon.svg" />
+      <link rel="icon" href="/favicon.ico" sizes="48x48" />
+      <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
       <link rel="manifest" href="/manifest.webmanifest" />
+      <meta name="theme-color" content="#202020" />
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,36 @@ const pwaConfig: Partial<VitePWAOptions> = {
   // seem to know how to deal with Vike), but we still need it to generate
   // `registerSW.js` for us.
   injectRegister: "script",
+
+  manifest: {
+    name: "My Vike App",
+    short_name: "My Vike App",
+    description: "Demo showcasing Vike",
+    theme_color: "#202020",
+    icons: [
+      {
+        src: "pwa-64x64.png",
+        sizes: "64x64",
+        type: "image/png",
+      },
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "maskable-icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  },
 };
 
 export default defineConfig({
