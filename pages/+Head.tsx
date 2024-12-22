2a3f70e939e19a968b2d788658770ed712289b02
dan-schel <me@danschellekens.com>
2024-12-22 16:49:17 +1100

    use online pwa assets generator instead

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -7,7 +7,7 @@ export default function HeadDefault() {
     <>
       <link rel="icon" href="/favicon.ico" sizes="48x48" />
       <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
-      <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <link rel="manifest" href="/manifest.webmanifest" />
       <meta name="theme-color" content="#202020" />
     </>
--- vite.config.ts
+++ vite.config.ts
@@ -16,24 +16,29 @@ const pwaConfig: Partial<VitePWAOptions> = {
     short_name: "My Vike App",
     description: "Demo showcasing Vike",
     theme_color: "#202020",
+
+    // Icons generated using https://favicon.inbrowser.app/tools/favicon-generator.
     icons: [
       {
-        src: "pwa-64x64.png",
-        sizes: "64x64",
-        type: "image/png",
-      },
-      {
-        src: "pwa-192x192.png",
+        src: "/pwa-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
       },
       {
-        src: "pwa-512x512.png",
+        src: "/pwa-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
       },
       {
-        src: "maskable-icon-512x512.png",
+        src: "/pwa-maskable-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/pwa-maskable-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
