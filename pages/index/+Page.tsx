15020ce42124d9d7547f058cb2ac7b7876cc8295
dan-schel <me@danschellekens.com>
2025-01-03 20:47:49 +1100

    box component

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -4,7 +4,7 @@ import { Counter } from "./Counter.js";
 export default function Page() {
   return (
     <>
-      <h1 className={"font-bold text-3xl pb-4"}>My Vike app</h1>
+      <h1 className={"pb-4 text-3xl font-bold"}>My Vike app</h1>
       This page is:
       <ul>
         <li>Rendered to HTML.</li>
