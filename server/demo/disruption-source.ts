e0cb8027c5bf5bca8abef43f4c6aaea1c75cef6b
XuanDienVo <xuan@tenurefi.com>
2025-01-12 16:16:39 +1100

    extends disruptions schema

--- server/demo/disruption-source.ts
+++ server/demo/disruption-source.ts
@@ -1,7 +1,48 @@
 import { z } from "zod";
 
+const directionSchema = z.object({
+  route_direction_id: z.number(),
+  direction_id: z.number(),
+  direction_name: z.string(),
+  service_time: z.string(),
+});
+
+const stopSchema = z.object({
+  stop_id: z.number(),
+  stop_name: z.string().trim(),
+});
+
+const routeSchema = z.object({
+  route_type: z.number(),
+  route_id: z.number(),
+  route_name: z.string().trim(),
+  route_number: z.string(),
+  route_gtfs_id: z.string(),
+  direction: directionSchema.nullable(),
+});
+
+export const disruptionSchema = z.object({
+  disruption_id: z.number(),
+  title: z.string(),
+  url: z.string().url(),
+  description: z.string(),
+  disruption_status: z.enum(["Planned", "Current"]),
+  disruption_type: z.string(),
+  published_on: z.string().datetime(),
+  last_updated: z.string().datetime(),
+  from_date: z.string().datetime(),
+  to_date: z.string().datetime().nullable(),
+  routes: routeSchema.array(),
+  stops: stopSchema.array(),
+  colour: z.string(),
+  display_on_board: z.boolean(),
+  display_status: z.boolean(),
+});
+
+export type Disruption = z.infer<typeof disruptionSchema>;
+
 export abstract class DisruptionSource {
-  abstract fetchDisruptions(): Promise<string[]>;
+  abstract fetchDisruptions(): Promise<unknown[]>;
 }
 
 export class VtarDisruptionSource extends DisruptionSource {
@@ -12,11 +53,7 @@ export class VtarDisruptionSource extends DisruptionSource {
     super();
   }
 
-  async fetchDisruptions(): Promise<string[]> {
-    const disruptionSchema = z.object({
-      title: z.string(),
-    });
-
+  async fetchDisruptions(): Promise<Disruption[]> {
     const responseSchema = z.object({
       disruptions: z.object({
         general: disruptionSchema.array(),
@@ -37,12 +74,11 @@ export class VtarDisruptionSource extends DisruptionSource {
 
     const data = responseSchema.parse(await response.json());
 
-    // Join general, metro, and regional train disruptions into one array, and
-    // extract just the titles.
+    // Join general, metro, and regional train disruptions into one array
     const combined = [
-      ...data.disruptions.general.map((d) => d.title),
-      ...data.disruptions.metro_train.map((d) => d.title),
-      ...data.disruptions.regional_train.map((d) => d.title),
+      ...data.disruptions.general,
+      ...data.disruptions.metro_train,
+      ...data.disruptions.regional_train,
     ];
 
     // eslint-disable-next-line no-console
