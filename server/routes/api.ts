5341afeb77024870c255265ba0a2ed0ce34b944c
x1-xddv <x1.xddv@gmail.com>
2025-01-13 11:02:34 +1100

    remove whitelist

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -3,10 +3,9 @@ import cors, { type CorsOptions } from "cors";
 import DisruptionRouter from "./disruptions";
 
 // CORS enabled to prevent API abuse from origins outside our domain(s)
-const whitelist = ["http://localhost:3000", /\.isitbuses\.com$/];
 const corsOptions: CorsOptions = {
   origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
+    if (!origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
