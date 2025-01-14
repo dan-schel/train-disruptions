cbfed7f34141795b6e156cc3454c4c064aa59fde
x1-xddv <x1.xddv@gmail.com>
2025-01-14 19:50:37 +1100

    validation/typed middleware

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -1,6 +1,7 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import cors, { type CorsOptions } from "cors";
 import DisruptionRouter from "./disruptions";
+import { ValidationError } from "../../types/errors/validation";
 
 // CORS enabled to prevent API abuse from origins outside our domain(s)
 const corsOptions: CorsOptions = {
@@ -13,9 +14,28 @@ const corsOptions: CorsOptions = {
   },
 };
 
+/**
+ * Handles any errors recovered via `next()`
+ */
+const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _: NextFunction,
+) => {
+  if (err instanceof ValidationError) {
+    return res.status(err.status).json({ error: err.errors });
+  }
+
+  return res.status(400).json({ error: err.message });
+};
+
 const apiRouter = Router();
 
 apiRouter.use(cors(corsOptions));
+
 apiRouter.use("/disruptions", DisruptionRouter);
+
+apiRouter.use(errorHandler);
 
 export default apiRouter;
---
+++ server/routes/middleware/validate.ts
@@ -0,0 +1,73 @@
+import { ZodIssue, ZodSchema } from "zod";
+import { NextFunction, Request, Response } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import {
+  IValidationError,
+  ValidationError,
+} from "../../../types/errors/validation";
+
+type ValidationRequest<TParams, TQuery, TBody, TResBody> = {
+  params?: ZodSchema<TParams>;
+  query?: ZodSchema<TQuery>;
+  body?: ZodSchema<TBody>;
+  response?: ZodSchema<TResBody>;
+};
+
+/**
+ * Validates the request with the provided schemas and defines the type for the respective properties
+ */
+export const validateMiddleware = <
+  TParams = ParamsDictionary,
+  TQuery = qs.ParsedQs,
+  TBody = unknown,
+  TResBody = unknown,
+>({
+  params,
+  query,
+  body,
+}: ValidationRequest<TParams, TQuery, TBody, TResBody>) => {
+  return (
+    req: Request<TParams, TResBody, TBody, TQuery>,
+    res: Response<TResBody>,
+    next: NextFunction,
+  ) => {
+    try {
+      const errors: IValidationError = {};
+
+      if (params) {
+        const { success, error } = params.safeParse(req.params);
+        if (!success) {
+          errors.params = parseError(error.errors[0]);
+        }
+      }
+
+      if (query) {
+        const { success, error } = query.safeParse(req.query);
+        if (!success) {
+          errors.query = parseError(error.errors[0]);
+        }
+      }
+
+      if (body) {
+        const { success, error } = body.safeParse(req.body);
+        if (!success) {
+          errors.body = parseError(error.errors[0]);
+        }
+      }
+
+      // If there are any errors, return a bad request error
+      if (Object.values(errors).length) {
+        throw new ValidationError(errors);
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+const parseError = (error: ZodIssue) => ({
+  field: error.path.join("."),
+  message: error.message,
+});
