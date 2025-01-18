1a414f899f728fb75456c6240a3beffbdc3de2dc
dan-schel <me@danschellekens.com>
2025-01-18 14:31:08 +1100

    northern loop exit

--- components/ptv-map/ptv-geometry.ts
+++ components/ptv-map/ptv-geometry.ts
@@ -2,6 +2,7 @@ import {
   FLAGSTAFF,
   FLINDERS_STREET,
   MELBOURNE_CENTRAL,
+  NORTH_MELBOURNE,
   PARLIAMENT,
   RICHMOND,
   SOUTHERN_CROSS,
@@ -12,33 +13,71 @@ import {
   Geometry,
   interchangeMarker,
   Line,
+  Path,
   split,
   straight,
 } from "./geometry";
+
+const long45 = Math.cos(Math.PI / 4);
+const short45 = 1 - Math.cos(Math.PI / 4);
+const diagonal = 1 / Math.sqrt(2);
 
 function radiusOf45DegAngleBox(size: number) {
   return size / (-1 + Math.sin(Math.PI / 4) + Math.cos(Math.PI / 4));
 }
 
-function sizeOf45DegAngleBox(radius: number) {
-  return radius * (-1 + Math.sin(Math.PI / 4) + Math.cos(Math.PI / 4));
-}
+function richmondLoopExit(dimensions: {
+  width: number;
+  height: number;
+  directStraight: number;
+  directRadius: number;
+  directExtension: number;
+  loopExtension: number;
+}) {
+  const {
+    width,
+    height,
+    directStraight,
+    directRadius,
+    directExtension,
+    loopExtension,
+  } = dimensions;
 
-function exitLoopRadius(dr: number, ds: number, ls: number, off: number) {
   const cos45 = Math.cos(Math.PI / 4);
-  return (dr * cos45 + (ds - ls) / Math.sqrt(2) - off) / (1 - cos45);
-}
+  const off = width - directStraight;
+  const exitLoopRadius =
+    (directRadius * cos45 +
+      (directExtension - loopExtension) / Math.sqrt(2) -
+      off) /
+    (1 - cos45);
 
-function exitLoopStraight(
-  dr: number,
-  ds: number,
-  ls: number,
-  offX: number,
-  offY: number,
-) {
   const sin45 = Math.sin(Math.PI / 4);
-  const r = exitLoopRadius(dr, ds, ls, offX);
-  return offY + (1 - sin45) * dr + (ds - ls) / Math.sqrt(2) - r * sin45;
+  const r = exitLoopRadius;
+  const exitLoopStraight =
+    height +
+    (1 - sin45) * directRadius +
+    (directExtension - loopExtension) / Math.sqrt(2) -
+    r * sin45;
+
+  return [
+    straight({
+      min: exitLoopStraight,
+      max: exitLoopStraight,
+    }),
+    curve({ radius: exitLoopRadius, angle: -45 }),
+    straight({ min: loopExtension, max: loopExtension }),
+    split({
+      reverse: true,
+      split: [
+        straight({ min: directExtension, max: directExtension }),
+        curve({ radius: directRadius, angle: -45 }),
+        straight({
+          min: directStraight,
+          max: directStraight,
+        }),
+      ],
+    }),
+  ];
 }
 
 const gippslandLine: Line = {
@@ -69,9 +108,29 @@ const otherRegionalLines: Line = {
     // SOUTHERN_CROSS
     curve({ radius: 5, angle: 90 }),
     straight({ min: 2, max: 2 }),
-    straight({ min: 30, max: 30 }),
+    straight({ min: 25, max: 25 }),
+    split({
+      reverse: false,
+      split: [
+        straight({ min: 10 / Math.sqrt(2), max: 10 / Math.sqrt(2) }),
+        curve({ radius: 20, angle: -45 }),
+        straight({ min: 5, max: 5 }),
+      ],
+    }),
+    curve({ radius: 20, angle: -45 }),
+    straight({ min: 10, max: 10 }),
   ],
 };
+
+const x = 25 + 15 * short45 - 5 * diagonal;
+const y = 15 * long45 + 25 * diagonal;
+const w = 15 + x;
+const h = 35 + y;
+const r = (w - y) / (long45 - short45);
+const d = (w - r * long45) / diagonal;
+const r2 = 20;
+const l1 = (x - short45 * r2) / diagonal;
+const l2 = h - l1 * diagonal - long45 * r2;
 
 const northernGroup: Line = {
   x: 0,
@@ -84,16 +143,18 @@ const northernGroup: Line = {
     curve({ radius: 15, angle: 90 }),
     straight({ min: 10, max: 10 }),
     interchangeMarker({ id: SOUTHERN_CROSS }),
-    straight({ min: 20, max: 20 }),
+    straight({ min: l2, max: l2 }),
     curve({
-      radius: radiusOf45DegAngleBox(15),
+      radius: r2,
       angle: -45,
     }),
+    straight({ min: l1, max: l1 }),
     split({
       reverse: true,
       split: [
+        straight({ min: d, max: d }),
         curve({
-          radius: radiusOf45DegAngleBox(15),
+          radius: (w - y) / (long45 - short45),
           angle: -45,
         }),
         interchangeMarker({ id: FLAGSTAFF }),
@@ -108,6 +169,8 @@ const northernGroup: Line = {
         // FLINDERS_STREET
       ],
     }),
+
+    interchangeMarker({ id: NORTH_MELBOURNE }),
   ],
 };
 
@@ -140,7 +203,7 @@ const cliftonHillGroup: Line = {
       ],
     }),
     curve({ radius: 20, angle: -45 }),
-    straight({ min: 60, max: 60 }),
+    straight({ min: 20, max: 20 }),
   ],
 };
 
@@ -163,23 +226,13 @@ const burnleyGroup: Line = {
     straight({ min: 20, max: 20 }),
     curve({ radius: 25, angle: 90 }),
     // PARLIAMENT
-    straight({
-      min: exitLoopStraight(35, 10, 25, 5, 55),
-      max: exitLoopStraight(35, 10, 25, 5, 55),
-    }),
-    curve({ radius: exitLoopRadius(35, 10, 25, 5), angle: -45 }),
-    straight({ min: 25, max: 25 }),
-    split({
-      reverse: true,
-      split: [
-        straight({ min: 10, max: 10 }),
-        curve({ radius: 35, angle: -45 }),
-        straight({
-          min: 40,
-          max: 40,
-        }),
-        // FLINDERS_STREET
-      ],
+    ...richmondLoopExit({
+      width: 45,
+      height: 55,
+      directStraight: 40,
+      directRadius: 35,
+      directExtension: 10,
+      loopExtension: 25,
     }),
     interchangeMarker({ id: RICHMOND }),
   ],
@@ -204,23 +257,13 @@ const dandenongGroup: Line = {
     straight({ min: 20, max: 20 }),
     curve({ radius: 30, angle: 90 }),
     interchangeMarker({ id: PARLIAMENT }),
-    straight({
-      min: exitLoopStraight(30, 10, 20, 10, 60),
-      max: exitLoopStraight(30, 10, 20, 10, 60),
-    }),
-    curve({ radius: exitLoopRadius(30, 10, 20, 10), angle: -45 }),
-    straight({ min: 20, max: 20 }),
-    split({
-      reverse: true,
-      split: [
-        straight({ min: 10, max: 10 }),
-        curve({ radius: 30, angle: -45 }),
-        straight({
-          min: 40,
-          max: 40,
-        }),
-        // FLINDERS_STREET
-      ],
+    ...richmondLoopExit({
+      width: 50,
+      height: 60,
+      directStraight: 40,
+      directRadius: 30,
+      directExtension: 10,
+      loopExtension: 20,
     }),
     // RICHMOND,
   ],
@@ -237,7 +280,10 @@ const werribeeWilliamstown: Line = {
     curve({ radius: 40, angle: 90 }),
     straight({ min: 10, max: 10 }),
     interchangeMarker({ id: SOUTHERN_CROSS }),
-    straight({ min: 60, max: 60 }),
+    straight({ min: 35, max: 35 }),
+    curve({ radius: 15, angle: -45 }),
+    straight({ min: 10, max: 10 }),
+    interchangeMarker({ id: NORTH_MELBOURNE }),
   ],
 };
 
