1373e621ac309dafb55748be8829e87c030c2f19
dan-schel <me@danschellekens.com>
2025-01-20 20:34:42 +1100

    shared corridor util file

---
+++ components/ptv-map/geometry/utils-shared-corridors.ts
@@ -0,0 +1,8 @@
+export const richmondToSouthYarra = 20;
+export const southYarraToCaulfield = 50;
+export const caulfieldToClayton = 50;
+export const claytonToDandenong = 50;
+// etc.
+
+// Can also put coordinated radii here, e.g. at Dandenong, where the Gippsland
+// line needs +5 extra radius.
