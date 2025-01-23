bc50ab03bc5da47233587213940de7176bbfce3d
x1-xddv <x1.xddv@gmail.com>
2025-01-23 22:00:10 +1100

    Revert "adds grid template to tailwind config"
    This reverts commit 46af3c96f3d4c446f8cf22a7739bb179630e681d.

--- components/common/PageCenterer.tsx
+++ components/common/PageCenterer.tsx
@@ -7,7 +7,7 @@ export type PageCentererProps = {
 
 export function PageCenterer(props: PageCentererProps) {
   return (
-    <div className="grid-cols-center lg:grid-cols-center-lg grid">
+    <div className="grid grid-cols-[0_1fr_0] lg:grid-cols-[1fr_64rem_1fr]">
       <With gridColumn="2">{props.children}</With>
     </div>
   );
