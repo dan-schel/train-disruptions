81e1db86c9f2f26f2490226d9f2b068bcdf9da98
dan-schel <me@danschellekens.com>
2025-02-02 18:24:37 +1100

    todos

--- server/data/line-section.ts
+++ server/data/line-section.ts
@@ -1,13 +1,12 @@
 export class LineSection {
-  // This only works for branching lines if we use two separate LineSections.
-  //
-  // Also, it's gonna be fun when the Albury line has bus replacements from
-  // Broadmeadows to Southern Cross, but the Seymour and Shepparton lines are
-  // still running!
-  //   A: Some services replaced by buses between Broadmeadows and Southern Cross.
-  // TODO: I need to start writing up these edge cases in the docs.
   constructor(
     readonly stationA: number,
     readonly stationB: number | "the-city",
   ) {}
+
+  // TODO: Will have some sort of toMapSection() method, which takes in the line
+  // route and returns the data the map needs to highlight this section.
+
+  // TODO: Also some sort of validate() method? If we include a station not on
+  // the line, that's an issue!
 }
