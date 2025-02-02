699be6deadd224b1ed0bc95b9f74878af440bc0f
dan-schel <me@danschellekens.com>
2025-02-02 16:18:47 +1100

    add routes to lines

--- server/data/line.ts
+++ server/data/line.ts
@@ -1,19 +1,73 @@
+import * as station from "./station-ids";
+
 export class Line {
   readonly id: number;
   readonly name: string;
   readonly ptvIds: readonly number[];
+  readonly route: LineRoute;
 
   constructor({
     id,
     name,
     ptvIds,
+    route,
   }: {
     id: number;
     name: string;
     ptvIds: readonly number[];
+    route: LineRoute;
   }) {
     this.id = id;
     this.name = name;
     this.ptvIds = ptvIds;
+    this.route = route;
+  }
+}
+
+export abstract class LineRoute {}
+
+export class SimpleLineRoute extends LineRoute {
+  constructor(readonly stations: readonly number[]) {
+    super();
+  }
+}
+
+export class LoopLineRoute extends LineRoute {
+  constructor(readonly stations: readonly number[]) {
+    super();
+
+    const portalStation = stations[0];
+    if (
+      ![station.RICHMOND, station.JOLIMONT, station.NORTH_MELBOURNE].includes(
+        portalStation,
+      )
+    ) {
+      throw new Error(`Invalid portal station: ${portalStation}`);
+    }
+  }
+
+  getPortal() {
+    return this.stations[0];
+  }
+}
+
+export class BranchingLineRoute extends LineRoute {
+  constructor(
+    readonly shared: readonly number[],
+    readonly branchA: readonly number[],
+    readonly branchB: readonly number[],
+  ) {
+    super();
+  }
+}
+
+export class WerribeeLineRoute extends LineRoute {
+  // Shrug
+  constructor(
+    readonly cityToNewport: readonly number[],
+    readonly altonaLoop: readonly number[],
+    readonly lavertonToWerribee: readonly number[],
+  ) {
+    super();
   }
 }
