71fd2b53f5f92c13340b9e7bd36ea47f812d4ad3
x1-xddv <x1.xddv@gmail.com>
2025-02-14 21:15:56 +1100

    marquee text for smaller screens

--- components/common/LineButton.tsx
+++ components/common/LineButton.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Line } from "../../server/data/line";
 
-import { Button } from "../core/Button";
 import { Row } from "../core/Row";
+import { Grid } from "../core/Grid";
 import { Text } from "../core/Text";
+import { Button } from "../core/Button";
 import { MingcuteRightLine } from "../icons/MingcuteRightLine";
 
 // TODO: Pick color for each status
@@ -16,26 +17,20 @@ type LineButtonProps = {
 export function LineButton({ line }: LineButtonProps) {
   return (
     <Button href={`/line/${line.id}`}>
-      <Row align="center" justify="space-between" className="gap-2 p-1 lg:p-2">
+      <Grid columns="auto 1fr auto" align="center" className="gap-2 p-1 lg:p-2">
         <Row align="center" className="gap-2">
           <div className="size-2 rounded-full bg-green-500" />
           <Text oneLine style="custom" className="text-sm lg:text-base">
             {line.name}
           </Text>
         </Row>
-        <Row align="center" className="gap-2">
-
-          {/* TODO: Deal with situation where status text is too long. Marquee text? */}
-          <Text
-            oneLine
-            style="custom"
-            className="text-sm text-green-500 lg:text-base"
-          >
-            No reported disruptions
-          </Text>
-          <MingcuteRightLine />
-        </Row>
-      </Row>
+        <div className="overflow-hidden text-end">
+          <span className="marquee text-sm text-green-500 lg:text-base">
+            Long message in case the two station names are long
+          </span>
+        </div>
+        <MingcuteRightLine />
+      </Grid>
     </Button>
   );
 }
