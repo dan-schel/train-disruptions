c82c2d414b6cb2cc64816a6e23d10bcb26faa6e6
x1-xddv <x1.xddv@gmail.com>
2025-02-15 20:03:57 +1100

    component for disruptions

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -3,19 +3,16 @@ import { useData } from "vike-react/useData";
 
 import { Data } from "./+data";
 
-import { LineButton } from "./LineButton";
+import { Lines } from "./Lines";
+import { Disruptions } from "./Disruptions";
 import { Map } from "../../components/map/Map";
+import { Row } from "../../components/core/Row";
 import { With } from "../../components/core/With";
 import { Text } from "../../components/core/Text";
 import { Column } from "../../components/core/Column";
 import { Spacer } from "../../components/core/Spacer";
 import { PagePadding } from "../../components/common/PagePadding";
 import { PageCenterer } from "../../components/common/PageCenterer";
-import { SimpleButton } from "../../components/common/SimpleButton";
-import { Button } from "../../components/core/Button";
-import { Grid } from "../../components/core/Grid";
-import { MingcuteRightLine } from "../../components/icons/MingcuteRightLine";
-import { MingcuteCloseCircleFill } from "../../components/icons/MingcuteCloseCircleFill";
 
 export default function Page() {
   const { suburban, regional } = useData<Data>();
@@ -26,88 +23,33 @@ export default function Page() {
         <Column>
           <Text style="title">Is it buses?</Text>
           <Spacer h="4" />
+
           <Text>Melbourne&apos;s train disruptions, visualised</Text>
-          <Spacer h="8" />
+          <Spacer h="6" />
+
+          <Row align="center" className="gap-1.5">
+            <Text>Show</Text>
+            <select className="w-35 rounded border border-black">
+              <option value={"all"}>all disruptions</option>
+            </select>
+            <Text>occurring</Text>
+            <select className="w-30 rounded border border-black">
+              <option value={"now"}>right now</option>
+            </select>
+          </Row>
+          <Spacer h="4" />
+
           <With className="rounded-md border border-slate-200">
             <Map />
           </With>
-          <Spacer h="2" />
-          <Column className="divide-y-1 divide-slate-200">
-            <Button href="/disruption/5">
-              <Grid
-                columns="2rem 1fr 1.5rem"
-                align="center"
-                className="gap-2 p-2"
-              >
-                <div className="grid size-8 rounded-full bg-[#16b4e8]" />
-                <Column className="gap-1.5">
-                  <Text style="custom" className="text-xs">
-                    Buses replace trains
-                  </Text>
-                  <Text>Caulfield to Westall</Text>
-                  <Text style="custom" className="text-xs">
-                    Until further notice
-                  </Text>
-                </Column>
-                <MingcuteRightLine className="size-full" />
-              </Grid>
-            </Button>
-            <Button href="/disruption/1">
-              <Grid
-                columns="2rem 1fr 1.5rem"
-                align="center"
-                className="gap-2 p-2"
-              >
-                <div className="grid size-8 rounded-full bg-[#6c3b9f]" />
-                <Column className="gap-1.5">
-                  <Text style="custom" className="text-xs">
-                    Major delays
-                  </Text>
-                  <Text>Sandringham line</Text>
-                </Column>
-                <MingcuteRightLine className="size-full" />
-              </Grid>
-            </Button>
-            <Button href="/disruption/1">
-              <Grid
-                columns="2rem 1fr 1.5rem"
-                align="center"
-                className="gap-2 p-2"
-              >
-                <MingcuteCloseCircleFill className="size-8" />
-                <Column className="gap-1.5">
-                  <Text>Glenferrie station closed</Text>
-                  <Text style="custom" className="text-xs">
-                    Until last train on Sunday
-                  </Text>
-                </Column>
-                <MingcuteRightLine className="size-full" />
-              </Grid>
-            </Button>
-          </Column>
-          <Spacer h="8" />
-
-          <Column className="gap-2">
-            <Text style="custom" className="text-lg font-bold">
-              Suburban lines
-            </Text>
-            <Column className="divide-y-1 divide-slate-200">
-              {suburban.map((line) => (
-                <LineButton key={line.id} line={line} />
-              ))}
-            </Column>
-          </Column>
           <Spacer h="4" />
-          <Column className="gap-2">
-            <Text style="custom" className="text-lg font-bold">
-              Regional lines
-            </Text>
-            <Column className="divide-y-1 divide-slate-200">
-              {regional.map((line) => (
-                <LineButton key={line.id} line={line} />
-              ))}
-            </Column>
-          </Column>
+
+          <Disruptions />
+          <Spacer h="4" />
+
+          <Lines title="Suburban lines" lines={suburban} />
+          <Spacer h="4" />
+          <Lines title="Regional lines" lines={regional} />
         </Column>
       </PagePadding>
     </PageCenterer>
--- pages/index/LineButton.tsx
+++ pages/index/LineButton.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 import { Line } from "../../server/data/line";
 
@@ -7,28 +8,45 @@ import { Text } from "../../components/core/Text";
 import { Button } from "../../components/core/Button";
 import { MingcuteRightLine } from "../../components/icons/MingcuteRightLine";
 
-// TODO: Pick color for each status
-// oklch(77.48% 0.1544 78.87)
-// oklch(61.76% 0.2511 28.06)
-
-// TODO: Attach disruption status
 type LineButtonProps = {
   line: Line;
+  status: "clear" | "delays" | "buses";
 };
 
-export function LineButton({ line }: LineButtonProps) {
+export function LineButton({ line, status }: LineButtonProps) {
   return (
     <Button href={`/line/${line.id}`}>
-      <Grid columns="auto 1fr auto" align="center" className="gap-2 p-1 lg:p-2">
+      <Grid
+        columns="auto 1fr auto"
+        align="center"
+        className="gap-2 p-1 transition-colors group-active:bg-slate-200 lg:p-2"
+      >
         <Row align="center" className="gap-2">
-          <div className="size-2 rounded-full bg-[#14d64b]" />
+          <div
+            className={clsx(
+              "size-2 rounded-full",
+              status === "clear" && "bg-status-clear",
+              status === "delays" && "bg-status-delays",
+              status === "buses" && "bg-status-buses",
+            )}
+          />
           <Text oneLine style="custom" className="text-sm lg:text-base">
             {line.name}
           </Text>
         </Row>
         <div className="overflow-hidden text-end">
-          <span className="marquee text-sm text-[#14d64b] lg:text-base">
-            Long message in case the two station names are long
+          <span
+            className={clsx(
+              "marquee text-sm lg:text-base",
+              status === "clear" && "text-status-clear",
+              status === "delays" && "text-status-delays",
+              status === "buses" && "text-status-buses",
+            )}
+          >
+            {status === "clear" && "No reported disruption"}
+            {status === "delays" && "Major delays"}
+            {status === "buses" &&
+              "Long message of the two station to test marquee"}
           </span>
         </div>
         <MingcuteRightLine />
