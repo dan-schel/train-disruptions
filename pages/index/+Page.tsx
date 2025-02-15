88828a1e74989893f303a13b4fd207ea76697ef0
x1-xddv <x1.xddv@gmail.com>
2025-02-15 14:24:06 +1100

    moves line component

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -3,15 +3,19 @@ import { useData } from "vike-react/useData";
 
 import { Data } from "./+data";
 
+import { LineButton } from "./LineButton";
 import { Map } from "../../components/map/Map";
 import { With } from "../../components/core/With";
 import { Text } from "../../components/core/Text";
 import { Column } from "../../components/core/Column";
 import { Spacer } from "../../components/core/Spacer";
-import { LineButton } from "../../components/common/LineButton";
 import { PagePadding } from "../../components/common/PagePadding";
 import { PageCenterer } from "../../components/common/PageCenterer";
 import { SimpleButton } from "../../components/common/SimpleButton";
+import { Button } from "../../components/core/Button";
+import { Grid } from "../../components/core/Grid";
+import { MingcuteRightLine } from "../../components/icons/MingcuteRightLine";
+import { MingcuteCloseCircleFill } from "../../components/icons/MingcuteCloseCircleFill";
 
 export default function Page() {
   const { suburban, regional } = useData<Data>();
@@ -27,9 +31,59 @@ export default function Page() {
           <With className="rounded-md border border-slate-200">
             <Map />
           </With>
-          <Spacer h="8" />
-          <Column className="gap-4" align="left">
-            <SimpleButton href="/disruption/1" text="View a disruption" />
+          <Spacer h="2" />
+          <Column className="divide-y-1 divide-slate-200">
+            <Button href="/disruption/5">
+              <Grid
+                columns="2rem 1fr 1.5rem"
+                align="center"
+                className="gap-2 p-2"
+              >
+                <div className="grid size-8 rounded-full bg-[#16b4e8]" />
+                <Column className="gap-1.5">
+                  <Text style="custom" className="text-xs">
+                    Buses replace trains
+                  </Text>
+                  <Text>Caulfield to Westall</Text>
+                  <Text style="custom" className="text-xs">
+                    Until further notice
+                  </Text>
+                </Column>
+                <MingcuteRightLine className="size-full" />
+              </Grid>
+            </Button>
+            <Button href="/disruption/1">
+              <Grid
+                columns="2rem 1fr 1.5rem"
+                align="center"
+                className="gap-2 p-2"
+              >
+                <div className="grid size-8 rounded-full bg-[#6c3b9f]" />
+                <Column className="gap-1.5">
+                  <Text style="custom" className="text-xs">
+                    Major delays
+                  </Text>
+                  <Text>Sandringham line</Text>
+                </Column>
+                <MingcuteRightLine className="size-full" />
+              </Grid>
+            </Button>
+            <Button href="/disruption/1">
+              <Grid
+                columns="2rem 1fr 1.5rem"
+                align="center"
+                className="gap-2 p-2"
+              >
+                <MingcuteCloseCircleFill className="size-8" />
+                <Column className="gap-1.5">
+                  <Text>Glenferrie station closed</Text>
+                  <Text style="custom" className="text-xs">
+                    Until last train on Sunday
+                  </Text>
+                </Column>
+                <MingcuteRightLine className="size-full" />
+              </Grid>
+            </Button>
           </Column>
           <Spacer h="8" />
 
--- pages/index/LineButton.tsx
+++ pages/index/LineButton.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Line } from "../../server/data/line";
 
-import { Row } from "../core/Row";
-import { Grid } from "../core/Grid";
-import { Text } from "../core/Text";
-import { Button } from "../core/Button";
-import { MingcuteRightLine } from "../icons/MingcuteRightLine";
+import { Row } from "../../components/core/Row";
+import { Grid } from "../../components/core/Grid";
+import { Text } from "../../components/core/Text";
+import { Button } from "../../components/core/Button";
+import { MingcuteRightLine } from "../../components/icons/MingcuteRightLine";
 
 // TODO: Pick color for each status
+// oklch(77.48% 0.1544 78.87)
+// oklch(61.76% 0.2511 28.06)
 
 // TODO: Attach disruption status
 type LineButtonProps = {
@@ -19,13 +21,13 @@ export function LineButton({ line }: LineButtonProps) {
     <Button href={`/line/${line.id}`}>
       <Grid columns="auto 1fr auto" align="center" className="gap-2 p-1 lg:p-2">
         <Row align="center" className="gap-2">
-          <div className="size-2 rounded-full bg-green-500" />
+          <div className="size-2 rounded-full bg-[#14d64b]" />
           <Text oneLine style="custom" className="text-sm lg:text-base">
             {line.name}
           </Text>
         </Row>
         <div className="overflow-hidden text-end">
-          <span className="marquee text-sm text-green-500 lg:text-base">
+          <span className="marquee text-sm text-[#14d64b] lg:text-base">
             Long message in case the two station names are long
           </span>
         </div>
