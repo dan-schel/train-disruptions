c77717daf28896742e86abf003dea8eb023fe145
x1-xddv <x1.xddv@gmail.com>
2025-03-01 15:06:22 +1100

    Overview page (#37)

---
+++ pages/index/Lines.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Line } from "./+data";
+
+import { LineButton } from "./LineButton";
+import { Text } from "../../components/core/Text";
+import { Column } from "../../components/core/Column";
+
+type LinesProps = {
+  title: string;
+  lines: Line[];
+};
+
+export function Lines(props: LinesProps) {
+  return (
+    <Column className="gap-2">
+      <Text style="custom" className="text-lg font-bold">
+        {props.title}
+      </Text>
+      <Column className="divide-y-1 divide-slate-200">
+        {props.lines.map((line) => (
+          <LineButton
+            key={line.id}
+            line={line}
+            status={getRandomStatus(line)}
+          />
+        ))}
+      </Column>
+    </Column>
+  );
+}
+
+// For visualisation only
+function getRandomStatus(line: Line) {
+  const random = (8 * line.id) % 10;
+
+  if (random < 5) return "clear";
+  if (random < 8) return "delays";
+  return "buses";
+}
--- server/data/static/line.ts
+++ server/data/static/line.ts
@@ -1,25 +1,31 @@
 import { LineRoute } from "./line-routes/line-route";
 
+type LineGroup = "suburban" | "regional";
+
 export class Line {
   readonly id: number;
   readonly name: string;
   readonly ptvIds: readonly number[];
   readonly route: LineRoute;
+  readonly lineGroup: LineGroup;
 
   constructor({
     id,
     name,
     ptvIds,
     route,
+    lineGroup,
   }: {
     id: number;
     name: string;
     ptvIds: readonly number[];
     route: LineRoute;
+    lineGroup: LineGroup;
   }) {
     this.id = id;
     this.name = name;
     this.ptvIds = ptvIds;
     this.route = route;
+    this.lineGroup = lineGroup;
   }
 }
