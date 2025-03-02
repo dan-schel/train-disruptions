867169d3c6b7be7d76f70c85aa5c7aa1d3ef24e2
dan-schel <me@danschellekens.com>
2025-03-02 13:41:45 +1100

    add canonical line shape to snapshot test

--- server/data/static/line-routes/line-route.ts
+++ server/data/static/line-routes/line-route.ts
@@ -4,27 +4,27 @@ import { CanonicalLineShape } from "./canonical-line-shape";
 import { unique } from "@dan-schel/js-utils";
 
 export class LineRoute {
-  private readonly allRouteGraphPairs: readonly StationPair[];
+  private readonly _allRouteGraphPairs: readonly StationPair[];
 
-  constructor(private readonly canonicalLineShape: CanonicalLineShape) {
+  constructor(private readonly _canonicalLineShape: CanonicalLineShape) {
     // This assumes there's no such thing as a route graph pair that cannot be
     // removed by some section of the canonical line shape.
     //
     // If it does become the case one day that there's a group of route graph
     // pairs that exist entirely separate from the canonical line shape, we can
     // change this constructor to provide them separately.
-    this.allRouteGraphPairs = unique(
-      canonicalLineShape.edges.flatMap((e) => e.data.routeGraphPairs),
+    this._allRouteGraphPairs = unique(
+      _canonicalLineShape.edges.flatMap((e) => e.data.routeGraphPairs),
       (a, b) => a.equals(b),
     );
   }
 
   getAllPairs(): readonly StationPair[] {
-    return this.allRouteGraphPairs;
+    return this._allRouteGraphPairs;
   }
 
   getPairsInSection(lineSection: LineSection): StationPair[] {
-    return this.canonicalLineShape.getRouteGraphPairsBetween(
+    return this._canonicalLineShape.getRouteGraphPairsBetween(
       lineSection.a,
       lineSection.b,
     );
@@ -32,8 +32,8 @@ export class LineRoute {
 
   isValidSection(lineSection: LineSection): boolean {
     return (
-      this.canonicalLineShape.validBoundary(lineSection.a) &&
-      this.canonicalLineShape.validBoundary(lineSection.b)
+      this._canonicalLineShape.validBoundary(lineSection.a) &&
+      this._canonicalLineShape.validBoundary(lineSection.b)
     );
   }
 }
--- tests/server/data/static/line-routes/line-routes.test.ts
+++ tests/server/data/static/line-routes/line-routes.test.ts
@@ -1,22 +1,55 @@
 import { describe, expect, it } from "vitest";
 import { lines } from "../../../../../server/data/static/lines";
 import { stations } from "../../../../../server/data/static/stations";
+import { CanonicalLineShape } from "../../../../../server/data/static/line-routes/canonical-line-shape";
+import { LineSectionBoundary } from "../../../../../server/data/line-section";
 
 describe("Melbourne default line route edges", () => {
   it("should match the snapshot", () => {
     let output = "";
 
     for (const line of lines.all()) {
+      if (output !== "") {
+        output += "\n\n";
+      }
       output += "\n";
 
-      output += `${line.name}:\n`;
+      output += `[${line.name.toUpperCase()} LINE]\n`;
+      output += `\nAll route graph pairs:\n`;
       for (const edge of line.route.getAllPairs()) {
         const a = stations.require(edge.a).name;
         const b = stations.require(edge.b).name;
         output += `  ${a} -> ${b}\n`;
       }
+
+      // Cheating a bit here :)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const canonicalLineShape: CanonicalLineShape = (line.route as any)
+        ._canonicalLineShape;
+
+      output += `\nCanonical line shape:\n`;
+      for (const edge of canonicalLineShape.edges) {
+        const a = formatBoundary(edge.from);
+        const b = formatBoundary(edge.to);
+
+        const pairs = edge.data.routeGraphPairs.map((pair) => {
+          const a = stations.require(pair.a).name;
+          const b = stations.require(pair.b).name;
+          return `${a} -> ${b}`;
+        });
+
+        output += `  ${a} -> ${b}: [${pairs.join(", ")}]\n`;
+      }
     }
 
     expect(output).toMatchSnapshot();
   });
+
+  function formatBoundary(boundary: LineSectionBoundary) {
+    if (boundary === "the-city") {
+      return '"The City"';
+    } else {
+      return stations.require(boundary).name;
+    }
+  }
 });
