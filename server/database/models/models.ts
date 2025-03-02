321a36272988db973f9ad71f8e412f1d33510a1e
LaskarisD <90429626+LaskarisD@users.noreply.github.com>
2025-03-02 18:38:32 +1100

    historical database of ptv messages

--- server/database/models/models.ts
+++ server/database/models/models.ts
@@ -1,3 +1,5 @@
 import { CrayonModel } from "./crayons";
+import { HistoricalAlertModel } from "./historical-alert";
 
 export const CRAYONS = CrayonModel.instance;
+export const HISTORICAL_ALERTS = HistoricalAlertModel.instance;
