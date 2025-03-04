a8d156ebea0a71ba742040635e31de4896fb1825
Dan Schellekens <me@danschellekens.com>
2025-03-04 21:06:23 +1100

    High-level line sections (#38)

--- server/data/static/line-routes/line-route.ts
+++ server/data/static/line-routes/line-route.ts
@@ -1,30 +1,55 @@
-export type LineRouteStationType =
-  | "served"
-  | "always-express"
-  | "often-express";
+import { LineSection } from "../../line-section";
+import { StationPair } from "./station-pair";
+import { LineShape } from "./line-shape";
 
-const defaultStationType = "served" as const;
+/**
+ * Knows deeply about the stations this line serves, and in which manner. It's
+ * primary responsibility is to provide a list of station pairs which form the
+ * basis of the route graph edges this line offers. (See docs/line-routes.md for
+ * more info.)
+ */
+export class LineRoute {
+  private readonly _allRouteGraphPairs: readonly StationPair[];
 
-export type InformalLineRouteStation =
-  | LineRouteStation
-  | number
-  | { id: number; type?: LineRouteStationType };
+  constructor(private readonly _shape: LineShape) {
+    // This works, but feels a little weird.
+    //
+    // The line shape is designed to determine which route graph edges should be
+    // REMOVED when a certain line section is disrupted. But acknowledging that
+    // all edges can be removed, there should never be an edge on the route
+    // graph that isn't mentioned in the line shape. So, instead of specifying
+    // them separately, we can just get them from the line shape.
+    //
+    // However, we can't really re-define edges in the line shape to say "this
+    // edge in the line shape PROVIDES these route graph edges", because for
+    // regional lines, some line shape edges remove route graph edges that occur
+    // beyond them (since they'd have to travel through). For example, the
+    // Gippsland line has an edge from Flinders Street to Richmond, which if
+    // disrupted, removes the route graph edges from Pakenham to Flinders Street
+    // AND Pakenham to Southern Cross! It really does mean the edges it REMOVES.
+    //
+    // It also means this class is only a wrapper for the line shape, because it
+    // doesn't need to track route graph edges separately. Could it ever make
+    // sense for a line to have a route graph edge that isn't captured by the
+    // line shape? We don't have any examples at the moment!
+    this._allRouteGraphPairs = _shape.getAllRouteGraphPairs();
+  }
 
-export class LineRouteStation {
-  constructor(
-    readonly stationId: number,
-    readonly type: LineRouteStationType,
-  ) {}
+  getAllRouteGraphPairs(): readonly StationPair[] {
+    return this._allRouteGraphPairs;
+  }
 
-  static formalize(station: InformalLineRouteStation): LineRouteStation {
-    if (station instanceof LineRouteStation) {
-      return station;
-    }
-    if (typeof station === "number") {
-      return new LineRouteStation(station, defaultStationType);
-    }
-    return new LineRouteStation(station.id, station.type ?? defaultStationType);
+  getRouteGraphPairsInSection(lineSection: LineSection): StationPair[] {
+    return this._shape.getRouteGraphPairsBetween(lineSection.a, lineSection.b);
+  }
+
+  // TODO: Function to get all line shape nodes? Admin dashboard will need to
+  // know to populate dropdowns.
+
+  isValidSection(lineSection: LineSection): boolean {
+    return (
+      this._shape.isValidNode(lineSection.a) &&
+      this._shape.isValidNode(lineSection.b)
+    );
   }
 }
-
-export abstract class LineRoute {}
---
+++ tests/server/data/static/line-routes/line-routes.test.ts
@@ -0,0 +1,56 @@
+import { describe, expect, it } from "vitest";
+import { lines } from "../../../../../server/data/static/lines";
+import { stations } from "../../../../../server/data/static/stations";
+import {
+  LineShape,
+  LineShapeNode,
+} from "../../../../../server/data/static/line-routes/line-shape";
+
+describe("Melbourne default line route edges", () => {
+  it("should match the snapshot", () => {
+    let output = "";
+
+    for (const line of lines.all()) {
+      if (output !== "") {
+        output += "\n\n";
+      }
+      output += "\n";
+
+      output += `[${line.name.toUpperCase()} LINE]\n`;
+      output += `\nAll route graph pairs:\n`;
+      for (const edge of line.route.getAllRouteGraphPairs()) {
+        const a = stations.require(edge.a).name;
+        const b = stations.require(edge.b).name;
+        output += `  ${a} -> ${b}\n`;
+      }
+
+      // Cheating a bit here :)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const lineShape: LineShape = (line.route as any)._shape;
+
+      output += `\nLine shape edges:\n`;
+      for (const edge of lineShape.edges) {
+        const a = formatLineShapeNode(edge.from);
+        const b = formatLineShapeNode(edge.to);
+
+        const pairs = edge.data.routeGraphPairs.map((pair) => {
+          const a = stations.require(pair.a).name;
+          const b = stations.require(pair.b).name;
+          return `${a} -> ${b}`;
+        });
+
+        output += `  ${a} -> ${b}: [${pairs.join(", ")}]\n`;
+      }
+    }
+
+    expect(output).toMatchSnapshot();
+  });
+
+  function formatLineShapeNode(boundary: LineShapeNode) {
+    if (boundary === "the-city") {
+      return '"The city"';
+    } else {
+      return stations.require(boundary).name;
+    }
+  }
+});
