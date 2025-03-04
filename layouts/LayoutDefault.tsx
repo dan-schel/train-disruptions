f3da64bcdcabbffec8db5db34f19891d01a339fc
dan-schel <me@danschellekens.com>
2025-03-04 20:58:12 +1100

    update overview page to fix bug

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -6,6 +6,7 @@ import { DesktopNavBar } from "../components/navigation/DesktopNavBar";
 import { MobileNavBar } from "../components/navigation/MobileNavBar";
 import { Column } from "../components/core/Column";
 import { With } from "../components/core/With";
+import clsx from "clsx";
 
 export default function LayoutDefault({
   children,
@@ -16,8 +17,13 @@ export default function LayoutDefault({
     throw new Error("Layout expects one child.");
   }
 
+  // Prevents pages which cannot shrink down to `--page-min-width` (20rem) from
+  // affecting fixed position elements (e.g. the mobile nav bar).
+  const handleOverwidth =
+    "w-[max(100vw,var(--page-min-width))] overflow-x-hidden";
+
   return (
-    <Column className="min-h-screen w-[max(100vw,var(--page-min-width))] overflow-x-hidden">
+    <Column className={clsx("min-h-screen", handleOverwidth)}>
       <DesktopNavBar />
       <MobileNavBar />
       <With flexGrow="1" className="pb-16 md:pt-12 md:pb-0">
