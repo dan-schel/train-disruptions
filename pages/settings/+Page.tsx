7c3b0ad8246558ffed2ba59073e884bcbc218ff0
Naubaj <naubadj@gmail.com>
2025-03-05 21:37:15 +1100

    headings for the sections

--- pages/settings/+Page.tsx
+++ pages/settings/+Page.tsx
@@ -4,6 +4,7 @@ import { Text } from "../../components/core/Text";
 import { Column } from "../../components/core/Column";
 import { PageCenterer } from "../../components/common/PageCenterer";
 import { PagePadding } from "../../components/common/PagePadding";
+import { Spacer } from "../../components/core/Spacer";
 
 export default function Page() {
   return (
@@ -11,6 +12,24 @@ export default function Page() {
       <PagePadding>
         <Column>
           <Text style="title">Settings</Text>
+          <Spacer h="6" />
+
+          <Text style="custom" className="text-lg font-bold">
+            Start page
+          </Text>
+
+          <Spacer h="4" />
+
+          <Text style="custom" className="text-lg font-bold">
+            Disruptions to Show
+          </Text>
+
+          <Spacer h="4" />
+
+          <Text style="custom" className="text-lg font-bold">
+            Colour Theme
+          </Text>
+
         </Column>
       </PagePadding>
     </PageCenterer>
