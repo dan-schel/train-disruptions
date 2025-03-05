82c8d6bbf1037a56b01c01cc373b0d27ddffee2e
dan-schel <me@danschellekens.com>
2025-03-05 17:41:18 +1100

    remove page width lock

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -6,7 +6,6 @@ import { DesktopNavBar } from "../components/navigation/DesktopNavBar";
 import { MobileNavBar } from "../components/navigation/MobileNavBar";
 import { Column } from "../components/core/Column";
 import { With } from "../components/core/With";
-import clsx from "clsx";
 
 export default function LayoutDefault({
   children,
@@ -17,13 +16,8 @@ export default function LayoutDefault({
     throw new Error("Layout expects one child.");
   }
 
-  // Prevents pages which cannot shrink down to `--page-min-width` (20rem) from
-  // affecting fixed position elements (e.g. the mobile nav bar).
-  const handleOverwidth =
-    "w-[max(100vw,var(--page-min-width))] overflow-x-hidden";
-
   return (
-    <Column className={clsx("min-h-screen", handleOverwidth)}>
+    <Column className="min-h-screen">
       <DesktopNavBar />
       <MobileNavBar />
       <With flexGrow="1" className="pb-16 md:pt-12 md:pb-0">
