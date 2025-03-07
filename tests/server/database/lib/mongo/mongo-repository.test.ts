693411031e6bfb4b567433814d0be3b325285ded
dan-schel <me@danschellekens.com>
2025-03-07 11:43:54 +1100

    mongo migration handler tests

--- tests/server/database/lib/mongo/mongo-repository.test.ts
+++ tests/server/database/lib/mongo/mongo-repository.test.ts
@@ -22,9 +22,9 @@ describe("MongoRepository", () => {
 
   describe("find", () => {
     it("calls find correctly", async () => {
+      const collection = mock<Collection<ModelDocument>>();
       const cursor = mock<FindCursor<Document>>();
       cursor.toArray.mockResolvedValue([]);
-      const collection = mock<Collection<ModelDocument>>();
       collection.find.mockReturnValue(cursor);
       const repository = new MongoRepository(MUSICAL_INSTRUMENTS, collection);
 
@@ -37,9 +37,9 @@ describe("MongoRepository", () => {
     });
 
     it("applies sorting and limits", async () => {
+      const collection = mock<Collection<ModelDocument>>();
       const cursor = mock<FindCursor<Document>>();
       cursor.toArray.mockResolvedValue([]);
-      const collection = mock<Collection<ModelDocument>>();
       collection.find.mockReturnValue(cursor);
       const repository = new MongoRepository(MUSICAL_INSTRUMENTS, collection);
 
