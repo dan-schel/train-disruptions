178ff3d850e59090e1e3a6db4dfd8872c64a6fe2
dan-schel <me@danschellekens.com>
2025-03-08 09:23:24 +1100

    Fix bug causing line page to not render

--- pages/line/@id/+data.ts
+++ pages/line/@id/+data.ts
@@ -1,14 +1,44 @@
-import { DataAsync } from "vike/types";
-import { lines } from "../../../server/data/static/lines";
+import { PageContext } from "vike/types";
+import { parseIntNull } from "@dan-schel/js-utils";
+import { LineCollection } from "../../../server/data/static/line-collection";
 import { Line } from "../../../server/data/static/line";
 
-export { data };
 export type Data = {
-  line: Line | null;
+  line: {
+    name: string;
+  } | null;
 };
 
-const data: DataAsync = async (pageContext): Promise<Data> => {
-  const line = lines.get(parseInt(pageContext.routeParams.id));
+export function data(pageContext: PageContext): Data {
+  const { app } = pageContext.custom;
 
-  return { line };
-};
+  const line = tryGetLine(app.lines, pageContext.routeParams.id);
+
+  if (line == null) {
+    return {
+      line: null,
+    };
+  }
+
+  return {
+    line: {
+      name: line.name,
+    },
+  };
+}
+
+function tryGetLine(
+  lines: LineCollection,
+  idStr: string | null | undefined,
+): Line | null {
+  if (idStr == null) {
+    return null;
+  }
+
+  const id = parseIntNull(idStr);
+  if (id == null) {
+    return null;
+  }
+
+  return lines.get(id);
+}
