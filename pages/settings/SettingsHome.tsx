03986871f246727c856c8b7a63a81d6651b0774b
Naubaj <naubadj@gmail.com>
2025-03-11 19:57:37 +1100

    start page options has hover background + whole area is clickable

--- pages/settings/SettingsHome.tsx
+++ pages/settings/SettingsHome.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { Column } from "../../components/core/Column";
-import { Row } from "../../components/core/Row";
 import { Text } from "../../components/core/Text";
 import { Spacer } from "../../components/core/Spacer";
 import { Settings } from "../../shared/settings";
@@ -9,6 +8,24 @@ import { Settings } from "../../shared/settings";
 export type HomepageProps = {
   settings: Settings;
   setSettings: (settings: Settings) => void;
+};
+
+const homepageOptions = ["overview", "commute"] as const;
+
+const formattedHomepage: Record<
+  (typeof homepageOptions)[number],
+  { name: string; value?: string; default?: undefined | number }
+> = {
+  overview: {
+    name: "Overview",
+    value: "overview",
+    default: undefined,
+  },
+  commute: {
+    name: "My Commute",
+    value: "commute",
+    default: 0,
+  },
 };
 
 export function SettingsHome({ settings }: HomepageProps) {
@@ -20,55 +37,21 @@ export function SettingsHome({ settings }: HomepageProps) {
       <Spacer h="2" />
 
       <Column>
-        {settings.commute === null ? (
-          // No data, default to Overview
-          <>
-            <Row className="gap-2">
-              <input
-                type="radio"
-                id="start-page-1"
-                name="start-page"
-                value="overview"
-                defaultChecked
-              />
-              <label htmlFor="start-page-1"> Overview </label>
-            </Row>
-
-            <Row className="gap-2">
-              <input
-                type="radio"
-                id="start-page-2"
-                name="start-page"
-                value="commute"
-              />
-              <label htmlFor="start-page-2"> My Commute </label>
-            </Row>
-          </>
-        ) : (
-          // Not null
-          <>
-            <Row className="gap-2">
-              <input
-                type="radio"
-                id="start-page-1"
-                name="start-page"
-                value="overview"
-              />
-              <label htmlFor="start-page-1"> Overview </label>
-            </Row>
-
-            <Row className="gap-2">
-              <input
-                type="radio"
-                id="start-page-2"
-                name="start-page"
-                value="commute"
-                defaultChecked
-              />
-              <label htmlFor="start-page-2"> My Commute </label>
-            </Row>
-          </>
-        )}
+        {homepageOptions.map((options) => (
+          <label key={options} className="flex gap-2 hover:bg-gray-200">
+            <input
+              type="radio"
+              name="start-page"
+              value={formattedHomepage[options].value}
+              defaultChecked={
+                // used for only initial render -> resetting cookies does not update instantly (requires reload ATM)
+                typeof settings.commute?.a ===
+                typeof formattedHomepage[options].default
+              }
+            />
+            {formattedHomepage[options].name}
+          </label>
+        ))}
       </Column>
     </Column>
   );
