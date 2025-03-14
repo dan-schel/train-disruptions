8b78cd1a12f588998a48760ee472325e97278d82
x1-xddv <x1.xddv@gmail.com>
2025-03-14 17:31:04 +1100

    Discord - Start-up logger (#52)

--- main.ts
+++ main.ts
@@ -12,14 +12,22 @@ import { lines } from "@/server/data/static/lines";
 import { stations } from "@/server/data/static/stations";
 import { initDatabase } from "@/server/database/init-database";
 import { initDisruptionSource } from "@/server/disruption-source/init-disruption-source";
+import { initDiscordClient } from "@/server/discord/init-discord";
 
 await main();
 
 async function main() {
   const database = await initDatabase();
   const disruptionSource = initDisruptionSource();
+  const discordClient = initDiscordClient();
 
-  const app = new App(lines, stations, database, disruptionSource);
+  const app = new App(
+    lines,
+    stations,
+    database,
+    disruptionSource,
+    discordClient,
+  );
   await app.init();
 
   await startWebServer(app);
---
+++ server/database/models/deployment-logs.ts
@@ -0,0 +1,43 @@
+import { DatabaseModel } from "@/server/database/lib/general/database-model";
+import { z } from "zod";
+
+export class DeploymentLog {
+  constructor(
+    readonly id: string,
+    readonly commitHash: string,
+    readonly createdAt: Date,
+  ) {}
+}
+
+export class DeploymentLogModel extends DatabaseModel<
+  DeploymentLog,
+  string,
+  z.input<typeof DeploymentLogModel.schema>
+> {
+  static instance = new DeploymentLogModel();
+
+  private static schema = z.object({
+    commitHash: z.string(),
+    createdAt: z.date(),
+  });
+
+  private constructor() {
+    super("deployment-logs");
+  }
+
+  getId(item: DeploymentLog): string {
+    return item.id;
+  }
+
+  serialize(item: DeploymentLog): { commitHash: string; createdAt: Date } {
+    return {
+      commitHash: item.commitHash,
+      createdAt: item.createdAt,
+    };
+  }
+
+  deserialize(id: string, item: unknown): DeploymentLog {
+    const parsed = DeploymentLogModel.schema.parse(item);
+    return new DeploymentLog(id, parsed.commitHash, parsed.createdAt);
+  }
+}
