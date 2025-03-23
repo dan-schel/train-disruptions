125ffb4768260a9c3c23483cd7dbed2a483cc144
x1-xddv <x1.xddv@gmail.com>
2025-03-23 21:07:46 +1100

    undo changes to button

--- components/common/SimpleButton.tsx
+++ components/common/SimpleButton.tsx
@@ -33,11 +33,7 @@ export function SimpleButton(props: SimpleButtonProps) {
   if (layout === "tile") {
     return (
       <Button {...action} alt={props.alt}>
-        <Column
-          className={clsx("gap-2 px-4 py-2", theme)}
-          align="center"
-          justify="center"
-        >
+        <Column className={clsx("gap-2 px-4 py-2", theme)} align="center">
           {props.icon && <With className="-ml-0.5 text-2xl">{props.icon}</With>}
           {props.text && <Text align="center">{props.text}</Text>}
         </Column>
