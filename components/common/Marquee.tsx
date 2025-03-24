dfb6c89940a6a45f60ec47377798ea1031685a4f
dan-schel <me@danschellekens.com>
2025-03-24 22:21:40 +1100

    marquee updates + choose duration based on text length

--- components/common/Marquee.tsx
+++ components/common/Marquee.tsx
@@ -5,9 +5,35 @@ export type MarqueeProps = {
 };
 
 export function Marquee(props: MarqueeProps) {
+  const outerRef = React.useRef<HTMLDivElement>(null);
+  const innerRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    const outerDiv = outerRef.current;
+    const innerDiv = innerRef.current;
+    if (outerDiv == null || innerDiv == null) return;
+
+    const observer = new ResizeObserver(() => setupMarquee(outerDiv, innerDiv));
+    observer.observe(outerDiv);
+    observer.observe(innerDiv);
+    setupMarquee(outerDiv, innerDiv);
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <div className="overflow-hidden">
-      <div className="_marquee">{props.children}</div>
+    <div className="_marquee" ref={outerRef}>
+      <div ref={innerRef}>{props.children}</div>
     </div>
   );
+}
+
+function setupMarquee(outerDiv: HTMLDivElement, innerDiv: HTMLDivElement) {
+  outerDiv.classList.toggle(
+    "active",
+    innerDiv.clientWidth > outerDiv.clientWidth,
+  );
+  const duration = (innerDiv.clientWidth + outerDiv.clientWidth) * 0.03;
+  outerDiv.style.setProperty("--duration", `${duration}s`);
+  outerDiv.style.setProperty("--width", `${outerDiv.clientWidth}px`);
 }
