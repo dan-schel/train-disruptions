632d664273ee53ec89505f0cf16d1a470ce77d47
dan-schel <me@danschellekens.com>
2025-03-28 19:43:42 +1100

    update comment

--- components/navigation/CenterAccountingForNav.tsx
+++ components/navigation/CenterAccountingForNav.tsx
@@ -12,12 +12,12 @@ export function CenterAccountingForNav(props: CenterAccountingForNavProps) {
       className={clsx(
         "fixed left-1/2 -translate-x-1/2 -translate-y-1/2 transform-gpu",
 
-        // Center on the page, excluding the space used by the mobile nav bar
-        // (--spacing(16)).
+        // In mobile view, render 2rem higher than the center to account for the
+        // 4rem (16 Tailwind units) high mobile nav bar.
         "top-[calc((100%---spacing(16))*0.5)]",
 
-        // Center on the page, excluding the space used by the desktop nav bar
-        // (--spacing(12)).
+        // In desktop view, render 1.5rem higher than the center to account for
+        // the 3rem (12 Tailwind units) high mobile nav bar.
         "md:top-[calc((100%+--spacing(12))*0.5)]",
 
         props.className,
