2fe59458c737fde1f9ece8fdc933d90483be85cd
dan-schel <me@danschellekens.com>
2025-04-05 14:53:44 +1100

    let the migration begin!

--- scripts/generate-map-geometry/lib/blueprint/path-blueprint-piece/path-blueprint-piece.ts
+++
@@ -1,6 +0,0 @@
-import { PathPieceBuilder } from "@/scripts/generate-map-geometry/lib/builder/path-piece-builder/path-piece-builder";
-
-export abstract class PathBlueprintPiece {
-  abstract reverse(): PathBlueprintPiece;
-  abstract getBuilder(): PathPieceBuilder;
-}
--- scripts/generate-map-geometry/lib/builder/path-piece-builder/path-piece-builder.ts
+++
@@ -1,5 +0,0 @@
-import { PathBuilder } from "@/scripts/generate-map-geometry/lib/builder/path-builder";
-
-export abstract class PathPieceBuilder {
-  abstract build(builder: PathBuilder): void;
-}
