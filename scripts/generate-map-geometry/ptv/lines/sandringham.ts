cef738aac86d9d27bab5fd8172114188e70634f9
dan-schel <me@danschellekens.com>
2025-04-05 21:21:26 +1100

    sandringham + stony-point

--- scripts/generate-map-geometry/ptv/lines/sandringham.ts
+++ scripts/generate-map-geometry/ptv/lines/sandringham.ts
@@ -1,8 +1,11 @@
 import { flexi } from "@/scripts/generate-map-geometry/lib/dimensions/flexi-length";
-import { LineBlueprint } from "@/scripts/generate-map-geometry/lib/blueprint/line-blueprint";
-import { PathBlueprint } from "@/scripts/generate-map-geometry/lib/blueprint/path-blueprint";
+import { LineBuilder } from "@/scripts/generate-map-geometry/lib/line-builder";
+import {
+  curve,
+  straight,
+} from "@/scripts/generate-map-geometry/lib/segment-instructions";
 import { flindersStreetToRichmond } from "@/scripts/generate-map-geometry/ptv/segments/flinders-street-to-richmond";
-import { defaultRadius } from "@/scripts/generate-map-geometry/ptv/utils";
+import { defaultRadius, east } from "@/scripts/generate-map-geometry/ptv/utils";
 import * as loop from "@/scripts/generate-map-geometry/ptv/utils-city-loop";
 import { richmondToSouthYarra } from "@/scripts/generate-map-geometry/ptv/utils-shared-corridors";
 import { SANDRINGHAM as node } from "@/shared/map-node-ids";
@@ -12,22 +15,19 @@ const diagonalStraight = flexi(10, 20);
 const sandringhamStraight = flexi(40, 80);
 
 /** The Sandringham line (colored pink on the map). */
-export const sandringham = new LineBlueprint({
-  origin: loop.pos.flindersStreet(loop.line.sandringham),
-  angle: 0,
-  color: "pink",
-
-  path: new PathBlueprint()
-    .node(node.FLINDERS_STREET)
-    .add(flindersStreetToRichmond(loop.line.sandringham))
-    .node(node.RICHMOND)
-    .straight(richmondToSouthYarra)
-    .node(node.SOUTH_YARRA)
-    .curve(defaultRadius, 45)
-    .straight(divergeStraight)
-    .curve(defaultRadius, 45)
-    .straight(diagonalStraight)
-    .curve(defaultRadius, -45)
-    .straight(sandringhamStraight)
-    .node(node.SANDRINGHAM),
-});
+export const sandringham = new LineBuilder(
+  node.FLINDERS_STREET,
+  loop.pos.flindersStreet(loop.line.sandringham),
+  east,
+  "pink",
+)
+  .to(node.RICHMOND, flindersStreetToRichmond(loop.line.sandringham))
+  .to(node.SOUTH_YARRA, [straight(richmondToSouthYarra)])
+  .to(node.SANDRINGHAM, [
+    curve(defaultRadius, 45),
+    straight(divergeStraight),
+    curve(defaultRadius, 45),
+    straight(diagonalStraight),
+    curve(defaultRadius, -45),
+    straight(sandringhamStraight),
+  ]);
--- scripts/generate-map-geometry/ptv/lines/stony-point.ts
+++ scripts/generate-map-geometry/ptv/lines/stony-point.ts
@@ -1,19 +1,16 @@
 import { flexi } from "@/scripts/generate-map-geometry/lib/dimensions/flexi-length";
-import { LineBlueprint } from "@/scripts/generate-map-geometry/lib/blueprint/line-blueprint";
-import { PathBlueprint } from "@/scripts/generate-map-geometry/lib/blueprint/path-blueprint";
 import { STONY_POINT as node } from "@/shared/map-node-ids";
 import { frankstonStationPos } from "@/scripts/generate-map-geometry/ptv/lines/frankston";
+import { LineBuilder } from "@/scripts/generate-map-geometry/lib/line-builder";
+import { straight } from "@/scripts/generate-map-geometry/lib/segment-instructions";
+import { east } from "@/scripts/generate-map-geometry/ptv/utils";
 
 const stonyPointStraight = flexi(50, 100);
 
 /** The Stony Point line (colored green on the map). */
-export const stonyPoint = new LineBlueprint({
-  origin: frankstonStationPos("stony-point"),
-  angle: 0,
-  color: "green",
-
-  path: new PathBlueprint()
-    .node(node.FRANKSTON)
-    .straight(stonyPointStraight)
-    .node(node.STONY_POINT),
-});
+export const stonyPoint = new LineBuilder(
+  node.FRANKSTON,
+  frankstonStationPos("stony-point"),
+  east,
+  "green",
+).to(node.STONY_POINT, [straight(stonyPointStraight)]);
