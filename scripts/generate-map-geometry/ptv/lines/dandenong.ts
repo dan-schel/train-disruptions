a6efec64aa6379e0b4790bf968b412e4fbefaf92
dan-schel <me@danschellekens.com>
2025-04-05 19:58:58 +1100

    clifton-hill and dandenong

--- scripts/generate-map-geometry/ptv/lines/dandenong.ts
+++ scripts/generate-map-geometry/ptv/lines/dandenong.ts
@@ -1,9 +1,15 @@
 import { flexi } from "@/scripts/generate-map-geometry/lib/dimensions/flexi-length";
-import { LineBlueprint } from "@/scripts/generate-map-geometry/lib/blueprint/line-blueprint";
-import { PathBlueprint } from "@/scripts/generate-map-geometry/lib/blueprint/path-blueprint";
-import { flagstaffToParliament } from "@/scripts/generate-map-geometry/ptv/segments/flagstaff-to-melbourne-central";
+import { LineBuilder } from "@/scripts/generate-map-geometry/lib/line-builder";
+import {
+  curve,
+  straight,
+  turnBack,
+} from "@/scripts/generate-map-geometry/lib/segment-instructions";
+import { flagstaffToMelbourneCentral } from "@/scripts/generate-map-geometry/ptv/segments/flagstaff-to-melbourne-central";
+import { flindersStreetToRichmond } from "@/scripts/generate-map-geometry/ptv/segments/flinders-street-to-richmond";
 import { flindersStreetToSouthernCross } from "@/scripts/generate-map-geometry/ptv/segments/flinders-street-to-southern-cross";
-import { richmondLoopPortal } from "@/scripts/generate-map-geometry/ptv/segments/richmond-loop-portal";
+import { melbourneCentralToParliament } from "@/scripts/generate-map-geometry/ptv/segments/melbourne-central-to-parliament";
+import { parliamentToRichmond } from "@/scripts/generate-map-geometry/ptv/segments/parliament-to-richmond";
 import { southernCrossToFlagstaff } from "@/scripts/generate-map-geometry/ptv/segments/southern-cross-to-flagstaff";
 import { defaultRadius } from "@/scripts/generate-map-geometry/ptv/utils";
 import * as loop from "@/scripts/generate-map-geometry/ptv/utils-city-loop";
@@ -21,50 +27,42 @@ import { DANDENONG as node } from "@/shared/map-node-ids";
 
 const loopPortalStraight = flexi(20);
 const cranbourneStraight = flexi(30, 45);
+const loopLine = loop.line.dandenong;
 
 /**
  * The Cranbourne and Pakenham lines, a.k.a. the "Dandenong group" (colored
  * light blue/cyan on the map).
  */
-export const dandenong = new LineBlueprint({
-  origin: loop.pos.flindersStreet(loop.line.dandenong),
-  angle: 180,
-  color: "cyan",
-
-  path: new PathBlueprint()
-    .node(node.FLINDERS_STREET_LOOP)
-    .add(flindersStreetToSouthernCross(3, false))
-    .node(node.SOUTHERN_CROSS)
-    .add(southernCrossToFlagstaff(3))
-    .node(node.FLAGSTAFF)
-    .add(flagstaffToParliament(3, node.MELBOURNE_CENTRAL))
-    .node(node.PARLIAMENT)
-    .add(
-      richmondLoopPortal(
-        loop.line.dandenong,
-        loopPortalStraight,
-        node.RICHMOND,
-        node.FLINDERS_STREET_DIRECT,
-      ),
-    )
-    .straight(richmondToSouthYarra)
-    .node(node.SOUTH_YARRA)
-    .straight(southYarraToCaulfield)
-    .node(node.CAULFIELD)
-    .straight(caulfieldToClayton)
-    .node(node.CLAYTON)
-    .straight(claytonToDandenong)
-    .node(node.DANDENONG)
-    .split({
-      split: new PathBlueprint()
-        .curve(defaultRadius, 45)
-        .straight(cranbourneStraight)
-        .node(node.CRANBOURNE),
-    })
-    .straight(dandenongToHallamCurve)
-    .curve(hallamCurvePakenham, -45)
-    .straight(hallamToPakenham)
-    .node(node.PAKENHAM)
-    .straight(pakenhamToEastPakenham)
-    .node(node.EAST_PAKENHAM),
-});
+export const dandenong = new LineBuilder(
+  node.FLINDERS_STREET_LOOP,
+  loop.pos.flindersStreet(loopLine),
+  180,
+  "cyan",
+)
+  .to(node.SOUTHERN_CROSS, flindersStreetToSouthernCross(loopLine, false))
+  .to(node.FLAGSTAFF, southernCrossToFlagstaff(loopLine))
+  .to(node.MELBOURNE_CENTRAL, flagstaffToMelbourneCentral(loopLine))
+  .to(node.PARLIAMENT, melbourneCentralToParliament(loopLine))
+  .to(node.RICHMOND, parliamentToRichmond(loopLine, loopPortalStraight))
+  .split((l) =>
+    l.to(node.FLINDERS_STREET_DIRECT, [
+      turnBack(),
+      ...flindersStreetToRichmond(loopLine).reverse(),
+    ]),
+  )
+  .to(node.SOUTH_YARRA, [straight(richmondToSouthYarra)])
+  .to(node.CAULFIELD, [straight(southYarraToCaulfield)])
+  .to(node.CLAYTON, [straight(caulfieldToClayton)])
+  .to(node.DANDENONG, [straight(claytonToDandenong)])
+  .split((l) =>
+    l.to(node.CRANBOURNE, [
+      curve(defaultRadius, 45),
+      straight(cranbourneStraight),
+    ]),
+  )
+  .to(node.PAKENHAM, [
+    straight(dandenongToHallamCurve),
+    curve(hallamCurvePakenham, -45),
+    straight(hallamToPakenham),
+  ])
+  .to(node.EAST_PAKENHAM, [straight(pakenhamToEastPakenham)]);
---
+++ scripts/generate-map-geometry/ptv/segments/parliament-to-jolimont.ts
@@ -0,0 +1,22 @@
+import { flexi } from "@/scripts/generate-map-geometry/lib/dimensions/flexi-length";
+import {
+  curve,
+  SegmentInstruction,
+  straight,
+} from "@/scripts/generate-map-geometry/lib/segment-instructions";
+import * as loop from "@/scripts/generate-map-geometry/ptv/utils-city-loop";
+
+export const radiusReduction = flexi(5);
+
+/** The curve from Parliament to Jolimont. */
+export function parliamentToJolimont(): SegmentInstruction[] {
+  const parliamentPos = loop.pos.parliament(loop.line.cliftonHill);
+  const flindersStreetPos = loop.pos.flindersStreet(loop.line.cliftonHill);
+  const radius = loop.radius(loop.line.cliftonHill).minus(radiusReduction);
+
+  return [
+    straight(parliamentPos.verticalDistanceTo(flindersStreetPos).minus(radius)),
+    curve(radius, -90),
+    curve(radius, -45),
+  ];
+}
