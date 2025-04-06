bda326bf4584291019f1eb8e268cff82b8970dfc
x1-xddv <x1.xddv@gmail.com>
2025-04-06 21:55:14 +1000

    Passenger Line Page (#72)

--- pages/line/@id/+Page.tsx
+++ pages/line/@id/+Page.tsx
@@ -8,6 +8,11 @@ import { PageCenterer } from "@/components/common/PageCenterer";
 import { PagePadding } from "@/components/common/PagePadding";
 import { BackNavigation } from "@/components/navigation/BackNavigation";
 import { Calendar } from "@/components/calendar/Calendar";
+import { ActiveDisruption } from "@/pages/line/@id/ActiveDisruption";
+import { UpcomingDisruption } from "@/pages/line/@id/UpcomingDisruption";
+import { MingcuteCheckCircleFill } from "@/components/icons/MingcuteCheckCircleFill";
+import { Row } from "@/components/core/Row";
+import { With } from "@/components/core/With";
 
 export default function Page() {
   const { line } = useData<Data>();
@@ -15,15 +20,37 @@ export default function Page() {
   return (
     <Column>
       <BackNavigation name="Overview" href="/overview" />
-      <PageCenterer>
-        <PagePadding>
-          <Column className="gap-4">
+      <With flexGrow="1">
+        <PageCenterer>
+          <PagePadding>
             {line != null ? (
-              <>
+              <Column className="gap-4">
                 <Text style="megatitle">Is it buses...</Text>
                 <Text>
                   on the <b>{line.name}</b> line?
                 </Text>
+
+                {line.active.length > 0 ? (
+                  <Column>
+                    {line.active.map((x) => (
+                      <ActiveDisruption
+                        key={x.id}
+                        lineNumber={line.id}
+                        disruption={x}
+                      />
+                    ))}
+                  </Column>
+                ) : (
+                  <Column className="gap-3 py-2">
+                    <Row align="center" className="text-status-green gap-2">
+                      <MingcuteCheckCircleFill className="size-10" />
+                      <Text style="custom" className="text-xl">
+                        Trains are running
+                      </Text>
+                    </Row>
+                    <Text>There are no reported disruptions.</Text>
+                  </Column>
+                )}
 
                 <hr className="border-soft-border" />
 
@@ -32,13 +59,35 @@ export default function Page() {
 
                   <Calendar data={line.calendar} />
                 </Column>
-              </>
+
+                <hr className="border-soft-border" />
+
+                <Text style="subtitle">Upcoming disruptions</Text>
+                <Column className="divide-soft-border divide-y">
+                  {line.upcoming.length > 0 ? (
+                    line.upcoming.map((x) => (
+                      <UpcomingDisruption
+                        key={x.id}
+                        lineNumber={line.id}
+                        disruption={x}
+                      />
+                    ))
+                  ) : (
+                    <Text>There are no disruptions coming up!</Text>
+                  )}
+                </Column>
+              </Column>
             ) : (
-              <Text>We don&apos;t know about this line 😔</Text>
+              <Column align="center" justify="center" className="h-full gap-4">
+                <Text align="center">
+                  It looks like your train has gone of its rails because we
+                  couldn&apos;t find which line you&apos;ve ended up at.
+                </Text>
+              </Column>
             )}
-          </Column>
-        </PagePadding>
-      </PageCenterer>
+          </PagePadding>
+        </PageCenterer>
+      </With>
     </Column>
   );
 }
