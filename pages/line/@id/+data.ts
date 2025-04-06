b4d4db5b442af1887812f7827ebd61806bdeacfe
x1-xddv <x1.xddv@gmail.com>
2025-04-06 17:19:33 +1000

    splits disruptions between active and upcoming

--- pages/line/@id/+data.ts
+++ pages/line/@id/+data.ts
@@ -6,11 +6,39 @@ import { JsonSerializable } from "@/shared/json-serializable";
 import { CalendarData } from "@/shared/types/calendar-data";
 import { createCalendarData } from "@/server/data/disruption/period/utils/create-calendar-data";
 import { getDemoDisruptions } from "@/server/data/disruption/demo-disruptions";
+import { TimeRange } from "@/server/data/disruption/period/utils/time-range";
+import {
+  LineStatusIndicatorPriorities,
+  LineStatusIndicatorPriority,
+} from "@/server/data/disruption/writeup/disruption-writeup";
+import { Disruption } from "@/server/data/disruption/disruption";
+
+const statusColorMapping: Record<
+  LineStatusIndicatorPriority,
+  "text-status-green" | "text-status-yellow" | "text-status-red"
+> = {
+  "very-low": "text-status-yellow",
+  low: "text-status-yellow",
+  medium: "text-status-red",
+  high: "text-status-red",
+  hidden: "text-status-green",
+};
+
+export type LinePageData = {
+  id: string;
+  headline: string | null;
+  subject: string;
+  period: string | null;
+  colour: (typeof statusColorMapping)[LineStatusIndicatorPriority];
+};
 
 export type Data = {
   line: {
+    id: number;
     name: string;
     calendar: CalendarData;
+    active: LinePageData[];
+    upcoming: Omit<LinePageData, "colour">[];
   } | null;
 };
 
@@ -25,17 +53,77 @@ export function data(pageContext: PageContext): Data & JsonSerializable {
     };
   }
 
-  const disruptions = getDemoDisruptions(app).filter((x) =>
-    x.data.getImpactedLines(app).includes(line.id),
+  const disruptions = getDemoDisruptions(app)
+    .filter(
+      (x) =>
+        x.data.getImpactedLines(app).includes(line.id) &&
+        x.data.getWriteupAuthor().write(app, x).lineStatusIndicator.priority !==
+          "hidden" &&
+        x.period.intersects(new TimeRange(app.time.now(), null)),
+    )
+    .sort((a, b) => {
+      return (
+        LineStatusIndicatorPriorities.indexOf(
+          b.data.getWriteupAuthor().write(app, b).lineStatusIndicator.priority,
+        ) -
+        LineStatusIndicatorPriorities.indexOf(
+          a.data.getWriteupAuthor().write(app, a).lineStatusIndicator.priority,
+        )
+      );
+    });
+
+  const [today, future] = disruptions.reduce<[Disruption[], Disruption[]]>(
+    (groups, x) => {
+      if (x.period.occursAt(app.time.now())) {
+        groups[0].push(x);
+      } else {
+        groups[1].push(x);
+      }
+
+      return groups;
+    },
+    [[], []],
   );
+
+  const active = today.map((x) => {
+    const writeup = x.data.getWriteupAuthor().write(app, x);
+
+    return {
+      id: x.id,
+      headline: writeup.summary.headline,
+      subject: writeup.summary.subject,
+      period: writeup.summary.period,
+      colour: statusColorMapping[writeup.lineStatusIndicator.priority],
+    };
+  });
+
+  const upcoming = future
+    .sort(
+      (a, b) =>
+        (a.period.getFullyEncompassingTimeRange().start?.getTime() ?? 0) -
+        (b.period.getFullyEncompassingTimeRange().start?.getTime() ?? 0),
+    )
+    .map((x) => {
+      const writeup = x.data.getWriteupAuthor().write(app, x);
+
+      return {
+        id: x.id,
+        headline: writeup.summary.headline,
+        subject: writeup.summary.subject,
+        period: writeup.summary.period,
+      };
+    });
 
   return {
     line: {
+      id: line.id,
       name: line.name,
       calendar: createCalendarData(
         disruptions.map((x) => x.period),
         app.time.now(),
       ),
+      active,
+      upcoming,
     },
   };
 }
