c0812861e353c41c46855937cc6d56336c25fba8
dan-schel <me@danschellekens.com>
2025-04-10 19:30:21 +1000

    some stations

--- server/data/station/station.ts
+++ server/data/station/station.ts
@@ -1,19 +1,27 @@
+import { MapPoint } from "@/server/data/map-point";
+
 export class Station {
   readonly id: number;
   readonly name: string;
   readonly ptvIds: readonly number[];
 
+  // TODO: [DS] Nullable temporarily.
+  readonly mapLocation: MapPoint | null;
+
   constructor({
     id,
     name,
     ptvIds,
+    mapLocation,
   }: {
     id: number;
     name: string;
     ptvIds: readonly number[];
+    mapLocation?: MapPoint;
   }) {
     this.id = id;
     this.name = name;
     this.ptvIds = ptvIds;
+    this.mapLocation = mapLocation ?? null;
   }
 }
