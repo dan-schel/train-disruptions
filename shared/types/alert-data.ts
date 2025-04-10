caba4fd3571a5d1d83b5f50d3b0ff38e94d48f7a
Naubaj <naubadj@gmail.com>
2025-04-10 17:24:19 +1000

    removes unneeded types

--- shared/types/alert-data.ts
+++ shared/types/alert-data.ts
@@ -1,24 +1,3 @@
-export type AlertData = {
-  title: string;
-  description: string;
-  url: string;
-  startsAt: Date | null;
-  endsAt: Date | null;
-  affectedLinePtvIds: number[];
-  affectedStationPtvIds: number[];
-};
-
-export type Alert = {
-  id: string;
-  data: AlertData;
-  updatedData: AlertData | null;
-  appearedAt: Date;
-  processedAt: Date | null;
-  updatedAt: Date | null;
-  ignoreFutureUpdates: boolean;
-  deleteAt: Date | null;
-}[];
-
 export type AlertPreview = {
   id: string;
   title: string;
