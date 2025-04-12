26949fc5ef038489fb147e9a872debaacf3c2725
x1-xddv <x1.xddv@gmail.com>
2025-04-12 15:46:52 +1000

    adds styled select component

---
+++ components/common/Select.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Text } from "@/components/core/Text";
+
+type SelectOption<T> = {
+  label: string;
+  value: T;
+};
+
+type SelectProps<T> = {
+  id?: string;
+  name?: string;
+  value?: T;
+  options: SelectOption<T>[];
+  handleChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+/**
+ * Styled select component
+ *
+ * To enforce the values of the options provided, provide the component with a type as follows
+ * `<Select<T> {...} />`
+ */
+export function Select<T extends string | number>({
+  id,
+  name,
+  options,
+  value,
+  handleChange,
+}: SelectProps<T>) {
+  return (
+    <select
+      id={id}
+      name={name}
+      value={value}
+      onChange={handleChange}
+      className="focus:border-b-accent-active hover:border-b-accent-hover border-b-soft-border h-6 cursor-pointer border-y-2 border-transparent focus:outline-0"
+    >
+      {options.map((x, i) => (
+        <option key={i} value={x.value} className="bg-background!">
+          <Text>{x.label}</Text>
+        </option>
+      ))}
+    </select>
+  );
+}
