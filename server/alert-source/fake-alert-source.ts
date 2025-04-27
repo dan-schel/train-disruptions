0a3aa0601094617a71536c2f4c3bff9a39bbd375
Dan Schellekens <me@danschellekens.com>
2025-04-27 14:59:52 +1000

    Remove old alerts when unpublished by PTV (#133)

--- server/alert-source/fake-alert-source.ts
+++ server/alert-source/fake-alert-source.ts
@@ -3,7 +3,13 @@ import { AlertSource } from "@/server/alert-source/alert-source";
 
 // For testing purposes.
 export class FakeAlertSource extends AlertSource {
+  private _alerts: Disruption[] = [];
+
+  setAlerts(alerts: Disruption[]) {
+    this._alerts = alerts;
+  }
+
   async fetchDisruptions(): Promise<Disruption[]> {
-    return [];
+    return this._alerts;
   }
 }
