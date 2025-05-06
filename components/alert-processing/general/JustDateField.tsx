b1cdb7eb7123734fdb189d188b9567b9f2592ba0
dan-schel <me@danschellekens.com>
2025-05-06 21:24:58 +1000

    general component todos

---
+++ components/alert-processing/general/JustDateField.tsx
@@ -0,0 +1,1 @@
+// TODO: A label, three text boxes in a row, and optional error label stacked in a column with a gap of 2.
---
+++ components/alert-processing/general/TextField.tsx
@@ -0,0 +1,1 @@
+// TODO: A label, text box, and optional error label stacked in a column with a gap of 2.
