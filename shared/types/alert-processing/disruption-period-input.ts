de3341f99d7fa1b0485efd3615a7cad4eeea3281
dan-schel <me@danschellekens.com>
2025-05-06 18:27:16 +1000

    disruption period input types

---
+++ shared/types/alert-processing/disruption-period-input.ts
@@ -0,0 +1,40 @@
+export type EndsAfterLastServiceInput = {
+  year: number;
+  month: number;
+  day: number;
+};
+
+export type EndsApproximatelyInputInput = {
+  displayText: string;
+  earliest: Date;
+  latest: Date;
+};
+
+export type EndsExactlyInput = {
+  date: Date;
+};
+
+export type EndsWhenAlertEndsInput = {
+  alertId: string;
+};
+
+export type EndsInput =
+  | ({ type: "ends-after-last-service" } & EndsAfterLastServiceInput)
+  | ({ type: "ends-approximately" } & EndsApproximatelyInputInput)
+  | ({ type: "ends-exactly" } & EndsExactlyInput)
+  | { type: "ends-never" }
+  | ({ type: "ends-when-alert-ends" } & EndsWhenAlertEndsInput);
+
+export type StandardDisruptionPeriodInput = {
+  start: Date;
+  end: EndsInput;
+};
+
+export type EveningsOnlyDisruptionPeriodInput = {
+  start: Date;
+  end: EndsInput;
+};
+
+export type DisruptionPeriodInput =
+  | ({ type: "standard" } & StandardDisruptionPeriodInput)
+  | ({ type: "evenings-only" } & EveningsOnlyDisruptionPeriodInput);
