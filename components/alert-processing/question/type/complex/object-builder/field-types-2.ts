b376443b3e7e146e77c9dad154bce94619f555e8
dan-schel <me@danschellekens.com>
2025-06-01 14:42:29 +1000

    continue sketch

--- components/alert-processing/question/type/complex/object-builder/field-types-2.ts
+++ components/alert-processing/question/type/complex/object-builder/field-types-2.ts
@@ -19,38 +19,54 @@ type Field<T, Extra> = {
   props: Extra;
 };
 
-type FieldValue<T> = T extends Field<infer V, any> ? V : never;
+type FieldValue<T extends Field<any, any>> =
+  T extends Field<infer V, any> ? V : never;
 
-type AdditionalProps<T> = T extends QuestionComponent<any, infer E> ? E : never;
+type AdditionalProps<T extends QuestionComponent<any, any>> =
+  T extends QuestionComponent<any, infer E> ? E : never;
 
-type ObjectValue<C> = {
+type KnownFieldTypes =
+  | Field<string, { label: string }>
+  | Field<number, { suffix: string }>
+  | Field<boolean, { format: "true-false" | "yes-no" }>;
+
+type AnyConfigType = { [key: string]: KnownFieldTypes };
+
+type ObjectValue<C extends AnyConfigType> = {
   [K in keyof C]: FieldValue<C[K]>;
 };
 
-type RawObjectValue<C> = {
+type RawObjectValue<C extends AnyConfigType> = {
   [K in keyof C]: Maybe<FieldValue<C[K]>>;
 };
 
 // ----------- EXAMPLE -----------
 
 type StringQuestionComponent = QuestionComponent<string, { label: string }>;
-type StringAdditionalProps = AdditionalProps<StringQuestionComponent>;
-type StringField = Field<string, StringAdditionalProps>;
-type StringValue = FieldValue<StringField>;
-
 const StringQuestion: StringQuestionComponent = ({ props }) => props.label;
 
-const stringField: StringField = {
-  type: StringQuestion,
-  props: { label: "First name" },
-};
+// prettier-ignore
+type BooleanQuestionComponent = QuestionComponent<boolean, { format: "true-false" | "yes-no" }>;
+const BooleanQuestion: BooleanQuestionComponent = ({ props }) => props.format;
+
+// prettier-ignore
+type ComplexQuestionComponent<T extends AnyConfigType> = QuestionComponent<ObjectValue<T>, { config: T }>;
+// prettier-ignore
+const ComplexQuestion: ComplexQuestionComponent<T> = <T>({ props }) => props.format; // Not sure how this could ever work...
 
 const config = {
   firstName: {
     type: StringQuestion,
-    props: { label: "First name" },
+    props: { label: "First Name" },
   },
-};
+  isDeleted: {
+    type: BooleanQuestion,
+    props: { format: "yes-no" },
+  },
+} satisfies AnyConfigType;
 
+type StringAdditionalProps = AdditionalProps<StringQuestionComponent>;
+type StringField = Field<string, StringAdditionalProps>;
+type StringValue = FieldValue<StringField>;
 type ConfigValue = ObjectValue<typeof config>;
 type RawConfigValue = RawObjectValue<typeof config>;
