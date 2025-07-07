b373692ebcf89431c4db095e3f40a6ba20bca863
dan-schel <me@danschellekens.com>
2025-07-07 10:56:22 +1000

    custom disruption data input

--- shared/types/alert-processing/disruption-data-input.ts
+++ shared/types/alert-processing/disruption-data-input.ts
@@ -1,12 +1,22 @@
+import { DisruptionWriteupInput } from "@/shared/types/alert-processing/disruption-writeup-input";
+import { MapHighlightingInput } from "@/shared/types/alert-processing/map-highlighting-input";
+import {
+  RouteGraphEdgeInput,
+  RouteGraphTrainEdgeInput,
+} from "@/shared/types/alert-processing/route-graph-edge-input";
+
 export type LineSectionInput = {
   line: number;
   a: number | "the-city";
   b: number | "the-city";
 };
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type CustomDisruptionDataInput = {
-  // TODO: [DS] Good luck!
+  impactedLines: number[];
+  writeup: DisruptionWriteupInput;
+  edgesToRemove: RouteGraphTrainEdgeInput[];
+  edgesToAdd: RouteGraphEdgeInput[];
+  highlighting: MapHighlightingInput;
 };
 
 export type StationClosureDisruptionDataInput = {
