28e8d0efb5410f39b7c58e2935476806164c5eb3
dan-schel <me@danschellekens.com>
2025-07-08 12:10:06 +1000

    ignore api

--- server/routes/admin/alert-processing/alert-processing.ts
+++ server/routes/admin/alert-processing/alert-processing.ts
@@ -1,37 +1,62 @@
 import { Router } from "express";
 import { validateMiddleware } from "@/server/routes/middleware/validate";
 import { App } from "@/server/app";
+import z from "zod";
+import { ALERTS } from "@/server/database/models/models";
 
-export function createAlertProcessingRouter(_app: App) {
+export function createAlertProcessingRouter(app: App) {
   const router = Router();
 
-  // Process an alert
   router.post(
     "/process",
     validateMiddleware({
+      // TODO: [DS] Implement validation.
       // params: z.whatever(),
       // query: z.whatever(),
       // body: z.whatever(),
     }),
-    async (_, res) => {
+    async (req, res) => {
+      // TODO: [DS] Implement processing API.
+
       res.json({
         // stuff
       });
     },
   );
 
-  // Ignore an alert
   router.post(
-    "/ignore",
+    "/ignore/:id",
     validateMiddleware({
-      // params: z.whatever(),
-      // query: z.whatever(),
-      // body: z.whatever(),
+      params: z.object({
+        id: z.string(),
+      }),
+      body: z.object({
+        permanently: z.boolean(),
+      }),
     }),
-    async (_, res) => {
-      res.json({
-        // stuff
-      });
+    async (req, res) => {
+      const id = req.params.id;
+      const alert = await app.database.of(ALERTS).get(id);
+
+      if (alert == null) {
+        res.sendStatus(404);
+        return;
+      }
+
+      if (alert.getState() !== "new") {
+        res.status(400).json({
+          error: "Alert already processed/ignored",
+        });
+        return;
+      }
+
+      if (req.body.permanently) {
+        await app.database.of(ALERTS).update(alert.ignored());
+      } else {
+        await app.database.of(ALERTS).update(alert.processed());
+      }
+
+      res.sendStatus(200);
     },
   );
 
