3454a5e47fc3305b9c7eebd66fbb53ef05acc71e
dan-schel <me@danschellekens.com>
2025-07-22 19:16:50 +1000

    fix name

--- components/alert-processing/AlertProcessingContextProvider.tsx
+++ components/alert-processing/AlertProcessingContextProvider.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import { AlertProcessingContext } from "@/components/alert-processing/use-alert-processing-context";
 import { AlertProcessingContextData } from "@/shared/types/alert-processing-context-data";
 
-export type SettingsProviderProps = {
+export type AlertProcessingContextProviderProps = {
   data: AlertProcessingContextData;
   children: React.ReactNode;
 };
 
-export function SettingsProvider(props: SettingsProviderProps) {
+export function AlertProcessingContextProvider(
+  props: AlertProcessingContextProviderProps,
+) {
   return (
     <AlertProcessingContext.Provider value={props.data}>
       {props.children}
