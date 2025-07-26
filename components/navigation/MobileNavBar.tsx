abec0a241f6b65ace838db93399b0b9d697c1ad5
Dan Schellekens <me@danschellekens.com>
2025-07-26 16:02:22 +1000

    Support edge-to-edge rendering on Chrome Android (#191)

--- components/navigation/MobileNavBar.tsx
+++ components/navigation/MobileNavBar.tsx
@@ -13,8 +13,11 @@ export function MobileNavBar() {
 function MobileNavBarLayout(props: NavBarOrchestratorLayoutProps) {
   const { nonSettingsTabs, settingsTab, isActiveTab, onTabClick } = props;
 
+  // Don't use a CSS var for `env(safe-area-inset-bottom, 0px)` because Chrome's
+  // code optimization for smooth animation doesn't seem to work unless it's
+  // hardcoded. (https://developer.chrome.com/docs/css-ui/edge-to-edge)
   return (
-    <nav className="bg-background border-t-soft-border fixed right-0 bottom-0 left-0 z-50 min-w-(--page-min-width) border-t md:hidden">
+    <nav className="bg-background border-t-soft-border fixed right-0 bottom-[calc(env(safe-area-inset-bottom,_0px)_-_var(--max-chin-size))] left-0 z-50 min-w-(--page-min-width) border-t pb-(--max-chin-size) md:hidden">
       <Grid className="auto-cols-[1fr] grid-flow-col px-4">
         {[...nonSettingsTabs, settingsTab].map((tab) => (
           <MobileTabButton
