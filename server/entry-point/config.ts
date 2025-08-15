572e2dd5be86e668752a872e5114d23f4f9fc017
dan-schel <me@danschellekens.com>
2025-08-15 20:20:21 +1000

    do not rename db

--- server/entry-point/config.ts
+++ server/entry-point/config.ts
@@ -1,7 +1,7 @@
 // Serves a similar purpose to env.ts, but for non-secret configuration values.
 
 const VTAR_URL = "https://vtar.trainquery.com";
-const DATABASE_NAME = "is-it-buses";
+const DATABASE_NAME = "train-disruptions";
 
 export const config = {
   DATABASE_NAME,
